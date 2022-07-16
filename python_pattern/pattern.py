f9693fcbfad88eb78189db6e6b1dd3de936afe1b
AjayKumar049 <92783730+AjayKumar049@users.noreply.github.com>
2022-07-16 04:27:58 -0400

    Add files via upload

---
+++ python_pattern/pattern.py
@@ -0,0 +1,17 @@
+for i in range(0, 5):
+    for j in range(0, i+1):
+        print("* ",end="")
+    print()
+# Python Numeric Pattern Example 2
+		
+for i in range(0, 5):
+    num = 1
+    for j in range(0, i+1):
+        print(num, end=" ")
+        num = num + 1
+    print()
+#Reverse Numeric pattern
+for i in range(5, 0, -1):
+    for j in range(0, i):
+        print(i, end="")
+    print()
